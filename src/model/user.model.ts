49abec34e6f82836536bd0493b879a4fae336a7d
FelixShih <zx8714245@gmail.com>
2024-04-30 01:42:26 +0800

    feat: modify the type and add the validate in mongoose
    See: https://mongoosejs.com/docs/typescript.html

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,20 +1,33 @@
-import { Document, Model, Schema, model } from 'mongoose';
+import { Model, Schema, model } from 'mongoose';
+import { validateEmail, validatePassword } from '../util/validator';
 
-export type UserDocument = Document & {
+export interface IUser {
   email: string;
   password: string;
-};
+}
 
-const UserSchema = new Schema(
+const UserSchema = new Schema<IUser>(
   {
     email: {
       type: Schema.Types.String,
       required: true,
+      validate: {
+        validator: function (value: unknown) {
+          return typeof value === 'string' ? validateEmail(value) : false;
+        },
+        message: (props) => `${props.value} is not a valid email`,
+      },
       unique: true,
     },
     password: {
       type: Schema.Types.String,
       required: true,
+      validate: {
+        validator: function (value: unknown) {
+          return typeof value === 'string' ? validatePassword(value) : false;
+        },
+        message: (props) => `${props.value} is not a valid password`,
+      },
     },
   },
   {
@@ -23,6 +36,6 @@ const UserSchema = new Schema(
   }
 );
 
-const User: Model<UserDocument> = model<UserDocument>('User', UserSchema);
+const User: Model<IUser> = model<IUser>('User', UserSchema);
 
 export default User;
