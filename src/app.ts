6c656091cfa8c694be20ead454344f688da05d35
Mia Mai <mia.mai@datax.io>
2024-05-05 01:05:21 +0800

    Revert "fix: env port and swc run script"
    This reverts commit feda0915410b210159ff17e7dc78aaef3d6394a0.

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import authRouter from './route/auth.route';
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use('/api', authRouter());
