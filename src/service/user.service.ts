7954c81ac247ec600b1ea0030ade893508c49f52
Mia Mai <cb121466@gmail.com>
2024-05-12 12:20:30 +0800

    fix: 🐛 use mongoose isValidObjectId function

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,10 +1,17 @@
+import { isValidObjectId } from 'mongoose';
 import User from '../model/user.model';
 import { UserSchemaModel } from '../type/user.type';
 import { CoreError } from '../util/error-handler';
 
 export const get = async (userId: string) => {
+  if (!isValidObjectId(userId)) {
+    throw new CoreError('Invalid user ID');
+  }
+
   const existingUser = await User.findById(userId).select('-password');
-  if (!existingUser) throw new CoreError('User not found');
+  if (!existingUser) {
+    throw new CoreError('User not found');
+  }
 
   return existingUser;
 };
@@ -15,13 +22,21 @@ export const edit = async (
   userId: string,
   requestBody: UserEditCredentials
 ) => {
+  if (!isValidObjectId(userId)) {
+    throw new CoreError('Invalid user ID');
+  }
+
   const existingUser = await User.findById(userId).select('-password');
-  if (!existingUser) throw new CoreError('User not found');
+  if (!existingUser) {
+    throw new CoreError('User not found');
+  }
 
   const validFields = Object.keys(requestBody).every(
     (field) => field in existingUser
   );
-  if (!validFields) throw new CoreError('Invalid field');
+  if (!validFields) {
+    throw new CoreError('Invalid field');
+  }
 
   existingUser.set(requestBody);
   await existingUser.save();
