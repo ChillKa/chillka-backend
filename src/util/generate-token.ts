bc2cbae3c40f5740e538fbb023ee9859929495ba
ben <cedarwud@gmail.com>
2024-05-18 11:33:32 +0800

    fix: 🐛 change jwt sign expiresIn to 1 day
    retrace folder swagger/build

--- src/util/generate-token.ts
+++ src/util/generate-token.ts
@@ -5,7 +5,7 @@ export const generateToken = (user: UserBase) =>
   jwt.sign(
     { id: user._id, username: user.displayName, email: user.email },
     process.env.JWT_SECRET!,
-    { expiresIn: '30d' }
+    { expiresIn: '1d' }
   );
 
 export default generateToken;
