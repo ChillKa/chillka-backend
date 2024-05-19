1fd4c221c7e302cee97ed53a642255f24ec20389
ben <cedarwud@gmail.com>
2024-05-19 23:19:01 +0800

    fix: 🐛 fix userRegister type and userToken type

--- src/type/user.type.ts
+++ src/type/user.type.ts
@@ -1,19 +1,28 @@
+import mongoose from 'mongoose';
+
 export enum GenderEnum {
   MALE = '男',
   FEMALE = '女',
 }
+
 export interface UserBase {
-  _id?: string;
   displayName: string;
   email: string;
   password: string;
-  confirmPassword?: string;
 }
+
+export type UserRegisterCredentials = UserBase & {
+  confirmPassword: string;
+};
+
+export type UserTokenCredentials = Omit<UserBase, 'password'> & {
+  _id: mongoose.Types.ObjectId;
+};
 
 export interface UserSchemaModel extends UserBase {
   realName?: string;
   birthday?: string;
-  gender?: 'MALE' | 'FEMALE';
+  gender?: GenderEnum;
   age?: number;
   introduction?: string;
   phoneAreaCode?: number;
@@ -22,6 +31,5 @@ export interface UserSchemaModel extends UserBase {
   address?: string;
 }
 
-export type UserRegisterCredentials = UserBase;
 export type UserLoginCredentials = Omit<UserBase, 'displayName'>;
 export type UserEditCredentials = Omit<UserSchemaModel, 'password'>;
--- src/util/generate-token.ts
+++ src/util/generate-token.ts
@@ -1,11 +1,14 @@
 import jwt from 'jsonwebtoken';
-import { UserBase } from '../type/user.type';
+import { UserTokenCredentials } from '../type/user.type';
 
-export const generateToken = (user: UserBase) =>
-  jwt.sign(
-    { id: user._id, username: user.displayName, email: user.email },
-    process.env.JWT_SECRET!,
-    { expiresIn: '1d' }
-  );
+export const generateToken = ({
+  _id,
+  displayName,
+  email,
+}: UserTokenCredentials) => {
+  const token = { _id, displayName, email };
+
+  jwt.sign(token, process.env.JWT_SECRET!, { expiresIn: '1d' });
+};
 
 export default generateToken;
