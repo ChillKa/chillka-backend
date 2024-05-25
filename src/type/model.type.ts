1421d9c8d478c2fc41ec1ae2903fb3357273be6e
cedarwud <61696135+cedarwud@users.noreply.github.com>
2024-05-25 16:27:39 +0800

    feat: 🎸 send email to reset password (#21)
    * feat: 🎸 send email to reset password
    
    * feat: 🎸 add verify email api
    
    * fix: 🐛 fix route get verify email get token method
    
    * fix: 🐛 change export type location

--- src/type/model.type.ts
+++ src/type/model.type.ts
@@ -1,6 +1,13 @@
+import { UserTokenCredentials } from './user.type';
+
 export enum SortEnum {
   ASC = 'asc',
   DES = 'des',
+}
+
+export interface AuthDecoded extends UserTokenCredentials {
+  int: number;
+  exp: number;
 }
 
 export type SortType = 'asc' | 'des';
