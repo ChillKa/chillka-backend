aa70c413ff5cb7ea05c42a124dfacb4ccde2bb65
ben <cedarwud@gmail.com>
2024-06-13 15:11:16 +0800

    fix: 🐛 fix reset password route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import 'dotenv/config';
 import express from 'express';
-import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 import swaggerUi from 'swagger-ui-express';
 import User from './model/user.model';
@@ -55,39 +54,6 @@ app.get('/api/demo', async (req, res) => {
   await User.updateMany({ isEmailValidate: false }, { isEmailValidate: true });
 
   res.send('success validate email');
-});
-
-// demo page until frontend page is ready
-app.get('/api/demo/reset-password', async (req, res) => {
-  // in production will redirect to 404 page
-  let responseCode = '404 page not found';
-
-  const token = req.query.validateCode as string;
-  try {
-    if (token) {
-      jwt.verify(token, process.env.JWT_SECRET!);
-
-      responseCode = `
-          此為重置密碼範例頁，等前端頁面好了之後，會再導向正確頁面<br><br>
-          <form method='POST' action='/api/demo/reset-password'>
-            <input name='password' type='password'>密碼</input>
-            <br>
-            <input name='confirmPassword' type='password'>確認密碼</input>
-            <br><br>
-          </form>
-          在 postman 輸入符合規範的密碼、確認密碼、以下的 token 後，就可以測試重置密碼功能
-          <br><br>
-          Token <br>
-          <textarea name='token' style="width:400px;height:100px">${token}</textarea>
-          <br><br>
-          <h1>此頁面在寄出重置密碼信的一天後失效</h1>
-        `;
-    }
-
-    res.send(responseCode);
-  } catch {
-    res.send(responseCode);
-  }
 });
 
 mongoose.connect(process.env.MONGODB_URL ?? '').then(() => {
