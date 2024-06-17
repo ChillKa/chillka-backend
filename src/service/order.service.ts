4729819bfea0ec05bd8d2539526ddb607fd00113
Mia Mai <cb121466@gmail.com>
2024-06-17 21:57:38 +0800

    feat: 🎸 get order detail (#51)
    * feat: 🎸 get order detail
    
    * fix: update populated activity key
    
    * fix: rename

--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -68,7 +68,28 @@ export const getOrderList = async ({
 
     return paginatedData;
   } catch (error) {
-    throw new CoreError('Create order failed.');
+    throw new CoreError('Get order list failed.');
+  }
+};
+
+export const getOrderDetail = async (orderId: string) => {
+  try {
+    const orderObjectId = new mongoose.Types.ObjectId(orderId);
+    const order = await Order.findById(orderObjectId)
+      .populate('activityId')
+      .select(['-ticketId'])
+      .lean();
+
+    if (!order) {
+      throw new CoreError('Order not found.');
+    }
+
+    const { activityId, ...rest } = order;
+    const updatedOrder = { ...rest, activity: activityId };
+
+    return updatedOrder;
+  } catch (error) {
+    throw new CoreError('Get order detail failed.');
   }
 };
 
@@ -85,7 +106,7 @@ export const cancelOrder = async ({ userId, orderId }: CancelOrderParams) => {
 
     return { message: 'Cancel order success.' };
   } catch (error) {
-    throw new CoreError('Create order failed.');
+    throw new CoreError('Cancel order failed.');
   }
 };
 
