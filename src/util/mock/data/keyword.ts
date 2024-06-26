1ca74d3826bdf101675f8c5dec571a2915c6fd30
ben <cedarwud@gmail.com>
2024-06-26 22:21:34 +0800

    feat: 🎸 temp

---
+++ src/util/mock/data/keyword.ts
@@ -0,0 +1,216 @@
+const keywords = [
+  {
+    content: '音樂節',
+    count: 2500,
+  },
+  {
+    content: '馬拉松',
+    count: 2300,
+  },
+  {
+    content: '手作市集',
+    count: 2100,
+  },
+  {
+    content: '美食祭',
+    count: 2000,
+  },
+  {
+    content: '藝術展',
+    count: 1900,
+  },
+  {
+    content: '慢跑活動',
+    count: 1800,
+  },
+  {
+    content: '攝影課程',
+    count: 1700,
+  },
+  {
+    content: '登山行程',
+    count: 1600,
+  },
+  {
+    content: '親子活動',
+    count: 1500,
+  },
+  {
+    content: '瑜珈工作坊',
+    count: 1400,
+  },
+  {
+    content: '電影放映',
+    count: 1300,
+  },
+  {
+    content: '夜市導覽',
+    count: 1200,
+  },
+  {
+    content: '品酒會',
+    count: 1100,
+  },
+  {
+    content: '茶道教學',
+    count: 1000,
+  },
+  {
+    content: 'DIY工作坊',
+    count: 950,
+  },
+  {
+    content: '桌遊聚會',
+    count: 900,
+  },
+  {
+    content: '戶外露營',
+    count: 850,
+  },
+  {
+    content: '旅遊講座',
+    count: 800,
+  },
+  {
+    content: '舞蹈課程',
+    count: 750,
+  },
+  {
+    content: '書法教學',
+    count: 700,
+  },
+  {
+    content: '寵物聚會',
+    count: 650,
+  },
+  {
+    content: '烘焙教室',
+    count: 600,
+  },
+  {
+    content: '電競比賽',
+    count: 550,
+  },
+  {
+    content: '演講活動',
+    count: 500,
+  },
+  {
+    content: '科技展覽',
+    count: 480,
+  },
+  {
+    content: '健身課程',
+    count: 460,
+  },
+  {
+    content: '文學讀書會',
+    count: 440,
+  },
+  {
+    content: '潛水體驗',
+    count: 420,
+  },
+  {
+    content: '農夫市集',
+    count: 400,
+  },
+  {
+    content: '醫療健康',
+    count: 380,
+  },
+  {
+    content: '戲劇表演',
+    count: 360,
+  },
+  {
+    content: '動漫展',
+    count: 340,
+  },
+  {
+    content: '園藝課程',
+    count: 320,
+  },
+  {
+    content: '歷史導覽',
+    count: 300,
+  },
+  {
+    content: '手機攝影',
+    count: 280,
+  },
+  {
+    content: '藝文講座',
+    count: 260,
+  },
+  {
+    content: '紡織工作坊',
+    count: 240,
+  },
+  {
+    content: '社交舞會',
+    count: 220,
+  },
+  {
+    content: '民俗文化',
+    count: 200,
+  },
+  {
+    content: '雕刻課程',
+    count: 180,
+  },
+  {
+    content: '環保活動',
+    count: 160,
+  },
+  {
+    content: '創意市集',
+    count: 140,
+  },
+  {
+    content: '老爺車展',
+    count: 120,
+  },
+  {
+    content: '飛行體驗',
+    count: 100,
+  },
+  {
+    content: '瑜珈比賽',
+    count: 90,
+  },
+  {
+    content: '音樂治療',
+    count: 80,
+  },
+  {
+    content: '茶道表演',
+    count: 70,
+  },
+  {
+    content: '武術課程',
+    count: 60,
+  },
+  {
+    content: '乒乓球賽',
+    count: 50,
+  },
+  {
+    content: '水彩畫班',
+    count: 40,
+  },
+  {
+    content: '獨木舟體驗',
+    count: 30,
+  },
+  {
+    content: '旗袍展示',
+    count: 20,
+  },
+  {
+    content: '烏克麗麗演奏',
+    count: 10,
+  },
+];
+
+export default keywords;
